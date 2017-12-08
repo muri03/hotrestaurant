a367785e0e7bc58543fab9fb79dcc0fa749e445a
Muriel <muriel.mangubat@gmail.com>
2017-12-08 06:45:38 -0600

    removed extra path folder

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({ extended: false}))
 
 app.use(bodyParser.json())
 
-require('../app/routing/api-routes.js')(app);
-require('../app/routing/html-routes.js')(app);
+require('./routing/api-routes.js')(app);
+require('./routing/html-routes.js')(app);
 
 app.listen(PORT, function(){
 	console.log("App listening on PORT: " + PORT);
